537d701819fce9ef3bd1c9441539ff53cf4f099b
dksshddl@naver.com <!ckdwhd12>
2018-11-20 17:10:58 +0900

    추가 경로를 나타내기위해 bottom sheet 추가
    Path.json parsing할때 오류존재 --> deserialize하기

---
+++ app/src/main/java/com/btl/hcj/myapplication/RouteHolder.java
@@ -0,0 +1,15 @@
+package com.btl.hcj.myapplication;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RouteHolder {
+    public TextView textView;
+    public ImageView imageView;
+
+    public RouteHolder(View root) {
+//        textView = root.findViewById(R.id.root_container);
+//        imageView = root.findViewById(R.id.showCustom);
+    }
+}
